ae4cd2ff47cc5ee4dbb2da1e82629c817e26b839
suyeon woo <50320497+yeonwooz@users.noreply.github.com>
2022-04-19 13:03:43 +0000

    템플릿 컴포넌트 렌더함수 및 setState 후 리렌더 함수구현

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let state = {}
+// 참고출처 : https://junilhwang.github.io/TIL/Javascript/Design/Vanilla-JS-Component/#_2-state-setstate-render
+
+let state = {
+    items: ['item1', 'item2', 'item3', 'item4']
+}
 // 상태관리 
 
 const app = document.getElementById('app')
@@ -6,9 +10,22 @@ const app = document.getElementById('app')
 
 const render = () => {
   // 렌더 메서드
+  const {items} = state
+  
+  // * 템플릿 리터럴 안에서 또다시 템플릿 리터럴을 써서, li태그까지 포함하여 조인한 문자열을 생성
+  app.innerHTML = `
+    <ul>
+      ${items.map(item => `<li>${item}</li>`).join('')}  
+    </ul>
+    <button id="append">추가</button>
+`
+  document.getElementById('append').addEventListener('click', function(){
+    setState({items: [...items, `item${items.length + 1}`]})  // items에 현재길이 + 1 을 붙인 새로운 아이템 추가
+  })
 }
 
-const setState = () => {
+const setState = (newState) => {
+  state = {...state, ...newState}
   render()
   // 상태변경 후 리렌더
 }
